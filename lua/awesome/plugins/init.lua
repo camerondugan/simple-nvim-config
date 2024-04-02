86df0b768872477cfb853104cc7754545598a011
Cameron Dugan <cameron.dugan@protonmail.com>
2024-04-01 22:04:31 -0400

    disable ohne-accidents welcomeOnStartup

--- lua/awesome/plugins/init.lua
+++ lua/awesome/plugins/init.lua
@@ -76,7 +76,7 @@ return {
   {
     'blumaa/ohne-accidents',
     config = function()
-      require('ohne-accidents').setup()
+      require('ohne-accidents').setup { welcomeOnStartup = false }
       vim.api.nvim_set_keymap('n', '<leader>oh', ':OhneAccidents<CR>', { noremap = true, silent = true })
     end,
   },
